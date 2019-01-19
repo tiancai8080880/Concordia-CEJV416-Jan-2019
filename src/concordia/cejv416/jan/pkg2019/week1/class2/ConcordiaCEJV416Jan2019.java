9e2fad23f41a42d2f223fa1dc18f6af73bc5c720
po_jiang <po_jiang@CCE-FB107-08W.concordia.ca>
2019-01-18 19:33:55 -0500

    first package git

---
+++ src/concordia/cejv416/jan/pkg2019/week1/class2/ConcordiaCEJV416Jan2019.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concordia.cejv416.jan.pkg2019.week1.class2;
+
+/**
+ *
+ * @author po_jiang
+ */
+public class ConcordiaCEJV416Jan2019 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
